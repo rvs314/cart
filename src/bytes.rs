e346f0c723cacbe14f68392065b4dd7a6048708c
Raffi Sanna <rvs314@protonmail.com>
2021-10-16 17:52:43 -0400

    No rust

--- src/bytes.rs
+++
@@ -1,2 +0,0 @@
-/// A string of bytes
-pub type ByteString = Vec<u8>;
--- src/tree.rs
+++
@@ -1,8 +0,0 @@
-/// An n-child tree where each leaf and branch
-/// is marked with a `Mark`, and each leaf holds
-/// a `Mark` and a `Payload`
-#[derive(Debug)]
-pub enum MarkedTree<Mark, Payload> {
-    Branch(Mark, Vec<MarkedTree<Mark, Payload>>),
-    Leaf(Mark, Payload)
-}
